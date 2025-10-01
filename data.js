f081c66e1f7f03634b00a28035cdbf3790e145ad
tadappi <iskyimaging@gmail.com>
2025-10-01 15:44:58 +0900

    add files

---
+++ data.js
@@ -0,0 +1,53 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-oomachi",
+      "name": "oomachi",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3600,
+      "initialViewParameters": {
+        "yaw": -0.5539057890640535,
+        "pitch": 0.11719741075284773,
+        "fov": 0.3820346386960207
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.3480750122223704,
+          "pitch": 0.27637355564418264,
+          "title": "メイプル",
+          "text": "link"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
